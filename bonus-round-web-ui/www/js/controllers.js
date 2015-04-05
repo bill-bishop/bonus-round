fcae21446d2ca1a3455cdce9d9c05b48f33301d0
william-bishop <admin@the402.net>
2015-04-05 16:14:25 -0700

    Added links to social tab

--- bonus-round-web-ui/www/js/controllers.js
+++ bonus-round-web-ui/www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
-
+.controller('HomeCtrl', function($scope, BASE_URL) {
+	$scope.baseUrl = BASE_URL;
 })
 
 .controller('GamesCtrl', function($scope, Games, BASE_URL) {
@@ -9,6 +9,26 @@ angular.module('starter.controllers', [])
 	Games.getGames().then(function (games) {
 		$scope.games = games;
 	});
+})
+
+.controller('SocialCtrl', function($scope) {
+	$scope.socialSites = [
+		{
+			name: "Facebook",
+			icon: "ion-social-facebook-outline",
+			url: "https://www.facebook.com/BonusRoundPhx"
+		},
+		{
+			name: "Twitter",
+			icon: "ion-social-twitter-outline",
+			url: "https://twitter.com/The_Bonus_Round"
+		},
+		{
+			name: "Instagram",
+			icon: "ion-social-instagram-outline",
+			url: "https://instagram.com/the_bonus_round/"
+		}
+	];
 })
 
 .controller('TabCtrl', function($scope) {
