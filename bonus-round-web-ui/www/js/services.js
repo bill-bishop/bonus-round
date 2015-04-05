5067fa93a7e45c7fb72fc4039c7e7fbdbd4ac59b
william-bishop <admin@the402.net>
2015-04-05 14:55:38 -0700

    Changed games list JSON structure

--- bonus-round-web-ui/www/js/services.js
+++ bonus-round-web-ui/www/js/services.js
@@ -2,9 +2,9 @@ angular.module('starter.services', [])
 
 .factory('Games', function($http, BASE_URL) {
   return {
-    getComingSoon: function () {
+    getGames: function () {
       return $http.get(BASE_URL + '/data/games/games_list.json').then(function (response) {
-        return response.data.comingSoon.gamesList;
+        return response.data.games;
       });
     }
   }
