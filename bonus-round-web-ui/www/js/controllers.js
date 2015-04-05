ef9d773bbdef4cd8290b4a6033f286c4f1f12608
william-bishop <admin@the402.net>
2015-04-05 10:01:07 -0700

    Removed unnecessary code

--- bonus-round-web-ui/www/js/controllers.js
+++ bonus-round-web-ui/www/js/controllers.js
@@ -6,19 +6,4 @@ angular.module('starter.controllers', [])
 })
 
 .controller('TabCtrl', function($scope) {
-	function createTab(title, tab, onIcon, offIcon) {
-		return {			
-			title: title,
-			tab: tab,
-			onIcon: onIcon,
-			offIcon: offIcon
-		};
-	}
-
-	$scope.tabs = [
-		createTab('Home', 'home', 'ion-beer', 'ion-ios-game-controller-a'),
-		createTab('Get Social', 'social', 'ion-thumbsup', 'ion-thumbsup'),
-		createTab('About Us', 'about', 'ion-ios-help', 'ion-ios-help-outline'),
-		createTab('High Scores', 'leaderboard', 'ion-trophy', 'ion-trophy')
-	];
 });
