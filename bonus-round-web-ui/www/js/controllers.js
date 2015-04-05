1f6a84e847419e56770dd58cb64c0f731bdc0dc9
william-bishop <admin@the402.net>
2015-04-05 10:20:13 -0700

    Added generic Data factory, removed more garbage code

--- bonus-round-web-ui/www/js/controllers.js
+++ bonus-round-web-ui/www/js/controllers.js
@@ -1,9 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope) {
+
+})
 
 .controller('LeaderboardCtrl', function($scope) {
+
 })
 
 .controller('TabCtrl', function($scope) {
+	
 });
